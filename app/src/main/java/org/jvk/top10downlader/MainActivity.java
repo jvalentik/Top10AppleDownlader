2ad5994a17bffe22c2470b93aae956a77ca4a570
Jan Valentik <jvalentik@sk.ibm.com>
2017-02-06 19:45:47 +0100

    RSS Feed download and display implementation

--- app/src/main/java/org/jvk/top10downlader/MainActivity.java
+++ app/src/main/java/org/jvk/top10downlader/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,6 +16,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String APPLE_RSS = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml";
+    private TextView xmlTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        xmlTextView = (TextView) findViewById(R.id.xmlTextView);
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute(APPLE_RSS);
     }
 
     @Override
@@ -47,15 +53,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
         private String fileContents;
 
         @Override
         protected String doInBackground(String... params) {
             fileContents = downloadXMLFile(params[0]);
             if (fileContents == null) {
-                Log.d("DownloadData", "Error downloading");
+                Log.d(TAG, "Error downloading");
             }
             return fileContents;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG, "Result was: " + result);
+            xmlTextView.setText(fileContents);
         }
 
         private String downloadXMLFile(String urlPath) {
@@ -64,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 URL url = new URL(urlPath);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 int response = connection.getResponseCode();
-                Log.d("DownloadData", "ResponseCode: " + response);
+                Log.d(TAG, "ResponseCode: " + response);
                 InputStream is = connection.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
                 int charRead;
@@ -78,9 +92,11 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return buffer.toString();
             } catch (IOException e) {
-                Log.d("DownloadData", "IO Exception reading data: " + e.getMessage());
-                return null;
+                Log.d(TAG, "IO Exception reading data: " + e.getMessage());
+            } catch (SecurityException e) {
+                Log.d(TAG, e.getMessage());
             }
+            return null;
         }
     }
 }
